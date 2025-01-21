8c920a9ecf1b2120ce0204a872d268f005296a6a
Stefanyyy15 <yuli.estefani.s15@gmail.com>
2025-01-21 12:59:37 -0500

    feat: :sparkles: Cambiamos el nombre de los servicios

--- Nesflis/src/main/java/com/example/Nesflis/NesflisApplication.java
+++ Nesflis/src/main/java/com/example/Nesflis/NesflisApplication.java
@@ -17,32 +17,7 @@ public class NesflisApplication {
             ConfigurableApplicationContext contexto = SpringApplication.run(NesflisApplication.class, args);
             ContenidoServicio contenidoService = contexto.getBean(ContenidoServicio.class);
             
-            
-        Genero gen1 = new Genero();
-        gen1.setNombre("Utopico");
-        Genero gen2 = new Genero();
-        gen2.setNombre("Violencia");
-
-        Reparto rep1 = new Reparto();
-        rep1.setNombre("Bruno Diaz");
-        Reparto rep2 = new Reparto();
-        rep2.setNombre("Chespirito");
-
-        List<Genero> generos = new ArrayList<>();
-        generos.add(gen1);
-        generos.add(gen2);
-
-        List<Reparto> actores = new ArrayList<>();
-        actores.add(rep1);
-        actores.add(rep2);
-
-        Contenido contenido = new Contenido();
-        contenido.setTitulo("Perros espias 3");
-        contenido.setDescripcion("3 espias perros y un malvado gato. ¿Quien ganará?");
-        contenido.setAnio_Estreno(2023);
-        contenido.setClasificacion("+18");
-
-        contenidoService.agregarContenido(contenido, generos, actores);
+          
 	}
 
 }
