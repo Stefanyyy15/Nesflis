05daae25ed418f2bceff040bc0731fb95fa82f3d
Stefanyyy15 <yuli.estefani.s15@gmail.com>
2025-01-21 12:20:49 -0500

    feat: :bug: Agregamos un problema en propiedades

--- Nesflis/src/main/java/com/example/Nesflis/NesflisApplication.java
+++ Nesflis/src/main/java/com/example/Nesflis/NesflisApplication.java
@@ -1,13 +1,48 @@
 package com.example.Nesflis;
 
+import com.example.Nesflis.dominio.servicio.ContenidoServicio;
+import com.example.Nesflis.persistencia.entidad.Contenido;
+import com.example.Nesflis.persistencia.entidad.Genero;
+import com.example.Nesflis.persistencia.entidad.Reparto;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class NesflisApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NesflisApplication.class, args);
+            ConfigurableApplicationContext contexto = SpringApplication.run(NesflisApplication.class, args);
+            ContenidoServicio contenidoService = contexto.getBean(ContenidoServicio.class);
+            
+            
+        Genero gen1 = new Genero();
+        gen1.setNombre("Utopico");
+        Genero gen2 = new Genero();
+        gen2.setNombre("Violencia");
+
+        Reparto rep1 = new Reparto();
+        rep1.setNombre("Bruno Diaz");
+        Reparto rep2 = new Reparto();
+        rep2.setNombre("Chespirito");
+
+        List<Genero> generos = new ArrayList<>();
+        generos.add(gen1);
+        generos.add(gen2);
+
+        List<Reparto> actores = new ArrayList<>();
+        actores.add(rep1);
+        actores.add(rep2);
+
+        Contenido contenido = new Contenido();
+        contenido.setTitulo("Perros espias 3");
+        contenido.setDescripcion("3 espias perros y un malvado gato. ¿Quien ganará?");
+        contenido.setAnio_Estreno(2023);
+        contenido.setClasificacion("+18");
+
+        contenidoService.agregarContenido(contenido, generos, actores);
 	}
 
 }
--- Nesflis/src/main/java/com/example/Nesflis/dominio/repositorio/PlanRepositorio.java
+++ Nesflis/src/main/java/com/example/Nesflis/dominio/repositorio/PlanRepositorio.java
@@ -7,8 +7,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
-
 @Repository
 public interface PlanRepositorio extends JpaRepository<Plan, Long>{
     List<Plan> findByCalidadVideo(String calidadVideo);    
