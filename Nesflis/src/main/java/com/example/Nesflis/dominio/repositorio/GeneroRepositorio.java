c5827154490235a625c56a3b6453b67e84aa7a43
L-salamanca <luisferperezs@hotmail.com>
2025-01-22 20:46:17 -0500

    fix: :bug: Consulta corregida
    Se eliminó la consulta de PlanRepositorio que generaba error y se remplazó por una nueva en GeneroRepositorio
    
    @L-Salamanca

--- Nesflis/src/main/java/com/example/Nesflis/dominio/repositorio/GeneroRepositorio.java
+++ Nesflis/src/main/java/com/example/Nesflis/dominio/repositorio/GeneroRepositorio.java
@@ -5,5 +5,5 @@ import com.example.Nesflis.persistencia.entidad.Genero;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GeneroRepositorio extends JpaRepository<Genero, Long>{
-    
+    List<Genero> findByNombre(String nombre);
 }
